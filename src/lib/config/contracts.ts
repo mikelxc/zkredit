f6380edd1446d5690ca4e687830da0220a8eb248
Xinchen Liu <mike000708@me.com>
2025-03-23 10:02:22 -0400

    feat: add contract addresses configuration for Sepolia network

---
+++ src/lib/config/contracts.ts
@@ -0,0 +1,11 @@
+import { sepolia } from 'viem/chains';
+
+export const contractAddresses = {
+  [sepolia.id]: {
+    ZKreditRegistry: '0x4b2c79367e640f537e823cb14683256a5de4cf83',
+    DefaultValidator: '0x817c41d4dd5bca085f31251da0c5d086fd9cc6e8',
+    ZKreditCore: '0x82cbe1be78f426a7f3092d2fd827fb7e73ede702',
+  }
+};
+
+export const entryPointAddress = '0x0000000071727De22E5E9d8BAf0edAc6f37da032';
